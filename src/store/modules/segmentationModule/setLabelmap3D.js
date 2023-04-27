bf9c2c25edcfe9437179ff0adb752d576c750e48
mkramers <mattkramers@gmail.com>
2023-04-26 22:54:40 -0400

    Remove debug print and bump version

--- src/store/modules/segmentationModule/setLabelmap3D.js
+++ src/store/modules/segmentationModule/setLabelmap3D.js
@@ -102,8 +102,6 @@ function setLabelmap3DByFirstImageId(
     colorOffset,
   };
 
-  console.log('ADDED', colorOffset);
-
   const labelmaps2D = brushStackState.labelmaps3D[labelmapIndex].labelmaps2D;
   const slicelengthInBytes = buffer.byteLength / numberOfFrames;
   const sliceLengthInUint16 = slicelengthInBytes / 2; // SliceLength in Uint16.
